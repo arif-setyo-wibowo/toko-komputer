f87badebadfe1c494dc7f7e565d3ff78bad2df91
Bayu Nashrullah <nashr.bayu77@gmail.com>
2023-03-02 18:12:44 +0700

    generate mcr for case, employee, gpu, mobo, psu, and procie

---
+++ app/Http/Controllers/GraphicCardController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GraphicCard;
+use Illuminate\Http\Request;
+
+class GraphicCardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GraphicCard $graphicCard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GraphicCard $graphicCard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GraphicCard $graphicCard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GraphicCard $graphicCard)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_03_02_110746_create_power_supplies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('power_supplies', function (Blueprint $table) {
+            $table->uuid('psuId')->primary();
+            $table->foreignUuid('brandId')->references('brandId')->on('brands');
+            $table->string('psuName',255);
+            $table->string('psuPower',255);
+            $table->string('psuCertification',255);
+            $table->string('psuEfficiency',255);
+            $table->string('psuCooling',255);
+            $table->string('psuModular',255);
+            $table->string('psuConnector',255);
+            $table->string('psuWarranty',255);
+            $table->string('psuPrice', 255);
+            $table->string('psuStock', 255);
+            $table->string('psuImage', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('power_supplies');
+    }
+};
