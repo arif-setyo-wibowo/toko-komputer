6eea478af563d2d5ffe5a1484655087c4ff6c919
rinoZakharia <mnfx14@gmail.com.com>
2023-03-10 12:47:22 +0700

    change page title

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -6,7 +6,7 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta name="X-CSRF-TOKEN" content="{{ csrf_token() }}">
 
-  <title>{{ $title }} - NiceAdmin Bootstrap Template</title>
+  <title>{{ $title }} - Admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
